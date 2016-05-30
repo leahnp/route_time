54a43432f2331d1be1f79b8934fd05ccae65c47f
Leah <leahnpetersen@gmail.com>
2016-05-30 12:54:09 -0700

    working on adding a new location

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,8 +1,21 @@
 require_relative '../../lib/GoogleDirectionsApiWrapper'
 
 class RoutesController < ApplicationController
-  def new
+  def new_location
+    @user = User.find(current_user.id)
+    @location = Location.new
   end
+
+  def create_location
+    location = Location.new(params[:location])
+    if location.save
+      redirect_to root  
+    else
+      boh
+    end
+  end
+
+
 
   def get_data
     # call wrapper
