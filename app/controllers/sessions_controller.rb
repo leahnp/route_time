0761b14e3a92b41912edca3c6facd6a2f60f1502
Leah <leahnpetersen@gmail.com>
2016-05-29 18:42:37 -0700

    added find or create to users model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    auth_hash = request.env['omniauth.auth']
+    # raise
+    @user = User.find_or_create_from_omniauth(auth_hash)
+    if @user
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      redirect_to root_path, notice: "Failed to save the user"
+    end
+  end
 end
